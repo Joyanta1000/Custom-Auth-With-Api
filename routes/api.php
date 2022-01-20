8696efea1f41779abf34bf7f8ad9e1d5456f602c
joyanta1000 <joyanta955@gmail.com>
2022-01-20 15:29:44 +0600

    change in api.php

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
